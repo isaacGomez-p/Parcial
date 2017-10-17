755fb71889537b3f25346f842043a4f556caee61
Unknown <jazminv181@hotmail.com>
2017-10-17 07:54:19 -0500

    comitt2

--- PanelDerecho.java
+++ PanelDerecho.java
@@ -13,21 +13,7 @@ import javax.swing.border.Border;
 public class PanelDerecho extends JPanel {
 	public PanelDerecho() {
 
-	    setLayout( new BorderLayout( ) );
-        JLabel imagen = new JLabel( );
-        ImageIcon icono = new ImageIcon( "images/logo1.png" );
-      
-        
-        imagen.setIcon( icono );
-        //Define el icono que mostrará este componente.
-        imagen.setHorizontalAlignment( JLabel.CENTER );
-        //Establece la alineación del contenido de la etiqueta a lo largo del eje X.
-        
-        add( imagen, BorderLayout.CENTER );
-        //ubicacion de mi imagen dentro del panel
-       
-        
-        setBackground( Color.WHITE);
+
 	
 	}
 	
--- PanelIzquierdo.java
+++ PanelIzquierdo.java
@@ -1,5 +1,26 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PanelIzquierdo  extends JPanel{
-
+	PanelIzquierdo(){
+    setLayout( new BorderLayout( ) );
+    JLabel imagen = new JLabel( );
+    ImageIcon icono = new ImageIcon( "images/retiroplata.png" );
+  
+    
+    imagen.setIcon( icono );
+    //Define el icono que mostrará este componente.
+    imagen.setHorizontalAlignment( JLabel.CENTER );
+    //Establece la alineación del contenido de la etiqueta a lo largo del eje X.
+    
+    add( imagen, BorderLayout.CENTER );
+    //ubicacion de mi imagen dentro del panel
+   
+    
+    setBackground( Color.WHITE);
+	}
 }
